6441826ed02765edfae32e75430bce2f93d73656
NITHISH07-r <nithishmkce@gmail.com>
2026-06-18 12:40:47 +0530

    Sixth Commit

---
+++ pgsql1.py
@@ -0,0 +1,36 @@
+import psycopg2
+
+# Connect to PostgreSQL
+conn = psycopg2.connect(
+    host="localhost",
+    database="student_record",   # change this
+    user="postgres",
+    password="nithish@07",
+    port="5432"
+)
+
+cursor = conn.cursor()
+
+# Insert query
+query = """
+INSERT INTO users (name, email)
+VALUES (%s, %s)
+"""
+
+data = ("ISHWARYA", "ishwarya@gmail.com")
+
+cursor.execute(query, data)
+conn.commit()
+
+print("Data inserted successfully")
+
+# Fetch data to verify
+cursor.execute("SELECT * FROM users")
+rows = cursor.fetchall()
+
+for row in rows:
+    print(row)
+
+# Close connection
+cursor.close()
+conn.close()
