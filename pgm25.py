7f50c2ba4e73e0b766fcb78db6cd90a002c32fd6
NITHISH07-r <nithishmkce@gmail.com>
2026-06-20 22:34:52 +0530

    first commit

--- pgm25.py
+++ pgm25.py
@@ -1,23 +1,29 @@
 a=input("enter the scale in which temperature is given: ")
 b=input("enter the scale in which temperature is to be changed: ")
 c=int(input("enter the temperature value: "))
-if a=="celsius" or a=="Celsius" or a=="CELSIUS" and b=="fahrenheit" or b=="Fahrenheit" or b=="FAHRENHEIT":
-    d=(c*(9/5))+32
-    print("temperature in fahrenheit from celsius: ",d)
-elif a=="fahrenheit" or a=="Fahrenheit" or a=="FAHRENHEIT" and b=="celsius" or b=="Celsius" or b=="CELSIUS":
-    d=(c-32)*(5/9)
-    print("temperature in celsius from fahrenheit: ",d)
-elif a=="celsius" or a=="Celsius" or a=="CELSIUS" and b=="kelvin" or b=="Kelvin" or b=="KELVIN":
-    d=c+273.15
-    print("temperature in kelvin from celsius: ",d)
-elif a=="kelvin" or a=="Kelvin" or a=="KELVIN" and b=="celsius" or b=="Celsius" or b=="CELSIUS":
-    d=c-273.15
-    print("temperature in celsius from kelvin: ",d)
-elif  a=="fahrenheit" or a=="Fahrenheit" or a=="FAHRENHEIT" and b=="kelvin" or b=="Kelvin" or b=="KELVIN":
-    d=((c-32)*(5/9))+273.15
-    print("temperature in kelvin from fahrenheit: ",d)
-elif a=="kelvin" or a=="Kelvin" or a=="KELVIN" and b=="fahrenheit" or b=="Fahrenheit" or b=="FAHRENHEIT":
-    d=(((c-273.5)*(9/5))+32)
-    print("temperature in fahrenheit from kelvin: ",d)
+if (a.lower() == "celsius") and (b.lower() == "fahrenheit"):
+    d = (c * (9/5)) + 32
+    print("temperature in fahrenheit from celsius:", d)
+
+elif (a.lower() == "fahrenheit") and (b.lower() == "celsius"):
+    d = (c - 32) * (5/9)
+    print("temperature in celsius from fahrenheit:", d)
+
+elif (a.lower() == "celsius") and (b.lower() == "kelvin"):
+    d = c + 273.15
+    print("temperature in kelvin from celsius:", d)
+
+elif (a.lower() == "kelvin") and (b.lower() == "celsius"):
+    d = c - 273.15
+    print("temperature in celsius from kelvin:", d)
+
+elif (a.lower() == "fahrenheit") and (b.lower() == "kelvin"):
+    d = ((c - 32) * (5/9)) + 273.15
+    print("temperature in kelvin from fahrenheit:", d)
+
+elif (a.lower() == "kelvin") and (b.lower() == "fahrenheit"):
+    d = ((c - 273.15) * (9/5)) + 32
+    print("temperature in fahrenheit from kelvin:", d)
+
 else:
     print("enter proper data")
